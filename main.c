ac00292f28463f2470665ff7eace6baa9aed5975
Heng Li <lh3@me.com>
2021-03-31 16:26:29 -0400

    expose topn

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ static void print_usage(FILE *fp, const mc_opt_t *opt)
 	fprintf(fp, "Usage: rmaxcut [options] <in.txt>\n");
 	fprintf(fp, "Options:\n");
 	fprintf(fp, "  -s INT      RNG seed [%lu]\n", (unsigned long)opt->seed);
+	fprintf(fp, "  -t INT      only consider top INT edges [%d]\n", opt->topn);
 	fprintf(fp, "  -p INT      rounds of perturbation [%d]\n", opt->n_perturb);
 	fprintf(fp, "  -f FLOAT    fraction to flip for perturbation [%.3g]\n", opt->f_perturb);
 }
@@ -21,10 +22,11 @@ int main(int argc, char *argv[])
 
 	mc_realtime();
 	mc_opt_init(&opt);
-	while ((c = ketopt(&o, argc, argv, 1, "s:p:f:", 0)) >= 0) {
+	while ((c = ketopt(&o, argc, argv, 1, "s:p:f:t:", 0)) >= 0) {
 		if (c == 's') opt.seed = atol(o.arg);
 		else if (c == 'p') opt.n_perturb = atoi(o.arg);
 		else if (c == 'f') opt.f_perturb = atof(o.arg);
+		else if (c == 't') opt.topn = atoi(o.arg);
 	}
 	if (o.ind == argc) {
 		print_usage(stdout, &opt);
